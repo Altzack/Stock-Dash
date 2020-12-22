082f894ceee56fde9a44a9db18176d2cab785057
Zack <altschulerzack@yahoo.com>
2020-12-22 10:35:07 -0800

    about page/mobile adjustments

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -1,5 +1,79 @@
 import React from 'react';
+import styled from 'styled-components/macro';
+import { useResponsive } from '../common/responsiveComponents';
 
-export default function About() {
-  return <div>about</div>;
+const ContentContainer = styled.div`
+  flex-direction: column;
+  font-weight: 200;
+  display: flex;
+  align-items: center;
+  margin: auto;
+  @media (max-width: 480px) {
+    font-size: 20px;
+    padding: 10px;
+  }
+
+  @media (min-width: 750px) {
+    font-size: 20px;
+    max-width: 650px;
+    padding: 10px;
+  }
+
+  @media (min-width: 1023px) {
+    max-width: 900px;
+    font-size: 30px;
+    padding: 10px;
+  }
+  @media (min-width: 1023px) {
+    max-width: 900px;
+    font-size: 30px;
+    padding: 10px;
+  }
+`;
+
+const PageContainer = styled.div`
+  padding-top: 42px;
+  font-family: Rubik;
+  padding: 15px;
+  display: flex;
+  font-size: 28px;
+  color: #fff;
+  align-items: center;
+  width: 100%;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-size: 50px;
+  font-weight: 300;
+  text-align: left;
+  letter-spacing: 3px;
+  text-transform: capitalize;
+  color: #fff;
+  margin-bottom: 10px;
+  @media (max-width: 480px) {
+    font-size: 30px;
+  }
+`;
+
+export default function LandingPage() {
+  const { isTabletOrMobile } = useResponsive();
+  return (
+    <PageContainer mobile={isTabletOrMobile}>
+      <Title>About Stock-dash</Title>
+      <br />
+      <ContentContainer>
+        Stock-dash was built as my second full stack capstone for Bloc.
+        <br />
+        <br />
+        The idea for Stock-dash came from my interest in trading stocks. The
+        dashboard allows you to get the latest news and pricing for a symbol, as
+        well as see the past month's price data visualized. With the watchlist
+        feature you can easily navigate to your top 5 and keep an eye on your
+        favorite tickers.
+        <br />
+        <br />
+      </ContentContainer>
+    </PageContainer>
+  );
 }
